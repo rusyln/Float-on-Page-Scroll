0418949a8f048504d0088b3c881b2e074b800234
rusyln <31722099+rusyln@users.noreply.github.com>
2024-02-15 15:58:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## INTRODUCTION
+
+The "Float on Page Scroll" module has been adapted from the source code available at: https://codepen.io/aravi-pen/pen/VMaRvX
+
+
+## INSTALLATION
+
+Install as you would normally install a contributed Drupal module.
+See: https://www.drupal.org/node/895232 for further information.
+
+
+## MAINTAINERS
+
+Current maintainers for Drupal 10:
+
+- RUSYL NARITO (RUS) - https://drupal.org/u/rusylnarito
+
+This module is implemented as a Drupal block.
---
+++ src/Plugin/Block/FloatBlock.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\float_on_page_scroll\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Render\FormattableMarkup;
+
+/**
+ * Provides a floating video block.
+ *
+ * @Block(
+ *   id = "float_on_page_scrool_example",
+ *   admin_label = @Translation("Floating Video"),
+ *   category = @Translation("Float on Page Scroll")
+ * )
+ */
+class FloatBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'src' => 'https://www.youtube.com/embed/B3vs2zgHwgA',
+      'width' => '560',
+      'height' => '315',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['src'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Source URL'),
+      '#default_value' => $this->configuration['src'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['src'] = $form_state->getValue('src');
+    $this->configuration['width'] = $form_state->getValue('width');
+    $this->configuration['height'] = $form_state->getValue('height');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Attach CSS file.
+    $build['#attached']['library'][] = 'float_on_page_scroll/float_on_page_scroll';
+
+    // Get configuration values.
+    $src = $this->configuration['src'];
+
+    // Build block content.
+    $iframe = '<section class="videowrapper ytvideo">
+    <a href="javascript:void(0);" class="close-button"></a>
+    <div class="gradient-overlay"></div>
+    <i class="fa fa-arrows-alt" aria-hidden="true"></i>
+    <iframe class="featured-video" width="560" height="315" src="' . $src . '?enablejsapi=1&amp;rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</section>';
+
+    // Wrap the iframe content in a FormattableMarkup object.
+    $build['#markup'] = new FormattableMarkup($iframe, []);
+
+    return $build;
+  }
+
+}
